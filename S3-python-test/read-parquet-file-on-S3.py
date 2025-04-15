be631b157bc3a6ccb37ed2064cd7c18a75f2ddc2
coussotc <charly.coussot@univ-grenoble-alpes.fr>
2025-04-15 15:47:23 +0200

    add analysis-ready parquet example in python

--- S3-python-test/read-parquet-file-on-S3.py
+++ S3-python-test/read-parquet-file-on-S3.py
@@ -1,20 +1,17 @@
-# import boto3
-# from botocore.client import Config
-import pyarrow as pa
 import pandas as pd
+import matplotlib.pyplot as plt
 
 # MinIO settings
 minio_endpoint = "http://localhost:9000"
 access_key = "test"
 secret_key = "testtest"
 bucket_name = "test"
-object_name = "rain.parquet"
+object_name = "rain.gzip.parquet"
 
 file_path = f"s3://{bucket_name}/{object_name}"
 
 storage_options = {"s3":{
     "endpoint_url":minio_endpoint,
-#    "anon": False,  # set to True if public
     "key": access_key,
     "secret": secret_key
 }}
@@ -24,7 +21,7 @@ start_time = pd.Timestamp("2005-07-09 00:00:00").timestamp()*1000 #1120942500000
 end_time = pd.Timestamp("2005-07-19 00:00:00").timestamp()*1000  #1121724000000
 
 # Read only needed columns (optional: saves bandwidth if you specify)
-columns = ["dateBegin", "value"]
+columns = ["dateEnd", "value"]
 
 # Read from Parquet over HTTP with lazy loading
 df = pd.read_parquet(
@@ -32,11 +29,11 @@ df = pd.read_parquet(
     engine="pyarrow",
     columns=columns,  # Optional: load only these columns
     storage_options=storage_options,
-    filters=[("dateBegin",">",start_time),(("dateEnd"),"<",end_time)]
+    filters=[("dateEnd",">",start_time),(("dateEnd"),"<",end_time)]
 )
-
-# Filter by time range (local filtering after minimal read)
-#filtered_df = df[(df["dateBegin"] >= start_time) & (df["dateBegin"] <= end_time)]
 
 # ✅ Output result
 print(df.head())
+print(df.tail())
+df.plot(x='dateEnd')
+plt.show()
